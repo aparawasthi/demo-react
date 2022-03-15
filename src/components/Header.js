505e8c3b985ab10d6404f3731fa1ae72bfa31048
Apar Awasthi <aparawasthi1@gmail.com>
2022-03-15 17:38:08 +0530

    Corrected sort functionality for fetched results

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,33 +4,42 @@ import { actions } from '../store';
 import './Header.css';
 
 function Header() {
-
-    const username = useSelector(state => state.username);
+    const username = useSelector((state) => state.username);
+    const sortBy = useSelector((state) => state.sortBy);
 
     const dispatch = useDispatch();
 
     const submitUsername = (event) => {
         //dispatch({type:'CHANGE', username:event.target.value});
         dispatch(actions.updateUsername(event.target.value));
-    }
+    };
 
     const handleSortBy = (event) => {
         dispatch(actions.updateSortBy(event.target.value));
-    }
+    };
 
     return (
-        <header className='header bg-primary flex flex-align-center'>
-            <div className='container flex'>
-                <select id='sortBy' className='form-field fs-400' name='sortBy' onChange={handleSortBy}>
-                    <option value='name-asc'>Name (A - Z)</option>
-                    <option value='name-desc'>Name (Z - A)</option>
-                    <option value='rank-up'>Rank ↑</option>
-                    <option value='rank-down'> Rank ↓</option>
+        <header className="header bg-primary flex flex-align-center">
+            <div className="container flex">
+                <select value={sortBy} id="sortBy" className="form-field fs-400" name="sortBy" placefolder="" onChange={handleSortBy}>
+                    <option value="default">Sort the result</option>
+                    <option value="name-asc">Name (A - Z)</option>
+                    <option value="name-desc">Name (Z - A)</option>
+                    <option value="rank-up">Rank ↑</option>
+                    <option value="rank-down"> Rank ↓</option>
                 </select>
-                <input id='username' className='form-field fs-400' type='text' value={username} onChange={submitUsername} name='username' placeholder='Search By Username' />
+                <input
+                    id="username"
+                    className="form-field fs-400"
+                    type="text"
+                    value={username}
+                    onChange={submitUsername}
+                    name="username"
+                    placeholder="Search By Username"
+                />
             </div>
         </header>
-    )
+    );
 }
 
-export default Header
+export default Header;
